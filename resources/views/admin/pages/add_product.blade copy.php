1fd2ce2d62092d127f716d49c59b92056ef57af2
mubinkhan <mubinkhan2549@gmail.com>
2024-01-29 13:13:17 +0500

    created dropzone today

--- resources/views/admin/pages/add_product.blade copy.php
+++ resources/views/admin/pages/add_product.blade copy.php
@@ -1,56 +1,6 @@
 @extends('admin.layouts.default')
 @section('title', 'Add Product')
 @section('content')
-
-<link rel="stylesheet" href="https://rawgit.com/enyo/dropzone/master/dist/dropzone.css" />
-<script src="https://rawgit.com/enyo/dropzone/master/dist/dropzone.js"></script>
-    <style>
-.dropzone{
-  width: 100%%
-  margin: 1%
-  border: 2px dashed #3498db !important
-  border-radius: 5px
-  transition: .2s
-  height: 20vh !important;
-}
-
-.dropzone.dz-drag-hover{
-  border: 2px solid #3498db !important
-
-}
-.dz-message.needsclick{
-      img
-    width: 50px
-    display: block
-    margin: auto
-    opacity: .6
-    margin-bottom: 15px
-}
-
-
-span.plus{
-
-
-  display: none
-.dropzone.dz-started .dz-message
-  display: inline-block !important
-  width: 120px
-  float: right
-  border: 1px solid rgba(238, 238, 238, 0.36)
-  border-radius: 30px
-  height: 120px
-  margin: 16px
-  transition: .2s
-  span.text
-    display: none
-  span.plus
-    display: block
-    font-size: 70px
-    color: #AAA
-    line-height: 110px
-}
-    </style>
-
     <!-- main stated -->
     <main id="main" class="main">
 
@@ -70,17 +20,8 @@ span.plus{
                 <div class=" col-md-6">
                     <div class="row gy-3">
                         <div class="col-sm-4  col-md-12">
-                                <div classs="dropzone">
-                                    <form action="/upload" class="dropzone needsclick" id="demo-upload">
-                                      <div class="dz-message needsclick">
-                                        <span class="text">
-                                        <img class="img-fluid" src="http://www.freeiconspng.com/uploads/------------------------------iconpngm--22.png" alt="Camera" />
-                                        Drop files here or click to upload.
-                                        </span>
-                                        <span class="plus">+</span>
-                                      </div>
-                                    </form>
-                                  </div>
+                            <img class="img-fluid product-image" src="{{ asset('assets/admin/img/productsssss.jpg') }}"
+                                alt="" style="width: 100%; height: 330px">
                         </div>
                         <div class="col-sm-4 col-md-2">
                             <img class="img-fluid product-image-thumb"
@@ -297,7 +238,7 @@ span.plus{
                     </form>
                 </div>
             </div>
-            {{-- <div class="file-input-container">
+            <div class="file-input-container">
                 <input class="file-input" type="file" name="user_pic" id="fileInput1">
                 <div class="upload-icon" onclick="document.getElementById('fileInput1').click()">
                   <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -305,7 +246,7 @@ span.plus{
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M16.1634 23.6195L22.3139 15.6658C22.6482 15.2368 22.767 14.741 22.6556 14.236C22.559 13.777 22.2768 13.3406 21.8534 13.0095L20.8208 12.1893C19.922 11.4744 18.8078 11.5497 18.169 12.3699L17.4782 13.2661C17.3891 13.3782 17.4114 13.5438 17.5228 13.6341C17.5228 13.6341 19.2684 15.0337 19.3055 15.0638C19.4244 15.1766 19.5135 15.3271 19.5358 15.5077C19.5729 15.8614 19.3278 16.1925 18.9638 16.2376C18.793 16.2602 18.6296 16.2075 18.5107 16.1097L16.676 14.6499C16.5868 14.5829 16.4531 14.5972 16.3788 14.6875L12.0185 20.3311C11.7363 20.6848 11.6397 21.1438 11.7363 21.5878L12.2934 24.0032C12.3231 24.1312 12.4345 24.2215 12.5682 24.2215L15.0195 24.1914C15.4652 24.1838 15.8812 23.9807 16.1634 23.6195ZM19.5955 22.8673H23.5925C23.9825 22.8673 24.2997 23.1886 24.2997 23.5837C24.2997 23.9795 23.9825 24.3 23.5925 24.3H19.5955C19.2055 24.3 18.8883 23.9795 18.8883 23.5837C18.8883 23.1886 19.2055 22.8673 19.5955 22.8673Z" fill="white"></path>
                   </svg>
                 </div>
-              </div> --}}
+              </div>
         </section>
 
     </main>
@@ -324,7 +265,5 @@ span.plus{
                 //   $(this).addClass('active')
             })
         })
-
-
     </script>
 @endPushOnce
