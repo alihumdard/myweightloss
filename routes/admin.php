db311fc6e14206135cd8658ab855e8c2220c479c
Ali Humdard <MikaAlex78@gmail.com>
2024-02-07 18:16:00 +0500

    updation in the questions

--- routes/admin.php
+++ routes/admin.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DefualtController;
-use App\Http\Controllers\Admin\SystemUsersController;
+use App\Http\Controllers\Admin\SystemController;
 
 
 Route::prefix('admin')->group(function () {
@@ -11,34 +11,34 @@ Route::prefix('admin')->group(function () {
     Route::get('/faq', [DefualtController::class, 'faq'])->name('admin.faq');
     Route::get('/contact', [DefualtController::class, 'contact'])->name('admin.contact');
 
-    Route::get('/doctors', [SystemUsersController::class, 'doctors'])->name('admin.doctors');
-    Route::match(['get','post'],'/addDoctor',   [SystemUsersController::class, 'add_doctor'])->name('admin.addDoctor');
-    Route::match(['get','post'],'/storeDoctor', [SystemUsersController::class, 'store_doctor'])->name('admin.storeDoctor');
+    Route::get('/doctors', [SystemController::class, 'doctors'])->name('admin.doctors');
+    Route::match(['get','post'],'/addDoctor',   [SystemController::class, 'add_doctor'])->name('admin.addDoctor');
+    Route::match(['get','post'],'/storeDoctor', [SystemController::class, 'store_doctor'])->name('admin.storeDoctor');
     
-    Route::get('/categories', [SystemUsersController::class, 'categories'])->name('admin.categories');
-    Route::match(['get','post'],'/addCategory', [SystemUsersController::class, 'add_category'])->name('admin.addCategory');
-    Route::match(['get','post'],'/storeCategory', [SystemUsersController::class, 'store_category'])->name('admin.storeCategory');
+    Route::get('/categories', [SystemController::class, 'categories'])->name('admin.categories');
+    Route::match(['get','post'],'/addCategory', [SystemController::class, 'add_category'])->name('admin.addCategory');
+    Route::match(['get','post'],'/storeCategory', [SystemController::class, 'store_category'])->name('admin.storeCategory');
 
-    Route::match(['get','post'],'/prodcuts', [SystemUsersController::class, 'prodcuts'])->name('admin.prodcuts');
-    Route::match(['get','post'],'/addProduct', [SystemUsersController::class, 'add_product'])->name('admin.addProduct');
-    Route::match(['get','post'],'/storeProduct', [SystemUsersController::class, 'store_product'])->name('admin.storeProduct');
+    Route::match(['get','post'],'/prodcuts', [SystemController::class, 'prodcuts'])->name('admin.prodcuts');
+    Route::match(['get','post'],'/addProduct', [SystemController::class, 'add_product'])->name('admin.addProduct');
+    Route::match(['get','post'],'/storeProduct', [SystemController::class, 'store_product'])->name('admin.storeProduct');
     
-    Route::get('/admins', [SystemUsersController::class, 'admins'])->name('admin.admins');
-    Route::match(['get','post'],'/addAdmin',   [SystemUsersController::class, 'add_admin'])->name('admin.addAdmin');
-    Route::match(['get','post'],'/storeAdmin', [SystemUsersController::class, 'store_admin'])->name('admin.storeAdmin');
-    Route::get('/users', [SystemUsersController::class, 'users'])->name('admin.users');
+    Route::get('/admins', [SystemController::class, 'admins'])->name('admin.admins');
+    Route::match(['get','post'],'/addAdmin',   [SystemController::class, 'add_admin'])->name('admin.addAdmin');
+    Route::match(['get','post'],'/storeAdmin', [SystemController::class, 'store_admin'])->name('admin.storeAdmin');
+    Route::get('/users', [SystemController::class, 'users'])->name('admin.users');
     
-    Route::get('/questions', [SystemUsersController::class, 'questions'])->name('admin.questions');
-    Route::match(['get','post'],'/addQuestion', [SystemUsersController::class, 'add_question'])->name('admin.addQuestion');
-    Route::match(['get','post'],'/storeQuestion', [SystemUsersController::class, 'store_question'])->name('admin.storeQuestion');
-    Route::get('/assignQuestion', [SystemUsersController::class, 'assign_question'])->name('admin.assignQuestion');
-    Route::match(['get','post'], '/getAssignQuestion', [SystemUsersController::class, 'get_assign_quest'])->name('admin.getAssignQuestion');
-    Route::match(['get','post'], '/storeAssignQuestion', [SystemUsersController::class, 'store_assign_quest'])->name('admin.storeAssignQuestion');
+    Route::get('/questions', [SystemController::class, 'questions'])->name('admin.questions');
+    Route::match(['get','post'],'/addQuestion', [SystemController::class, 'add_question'])->name('admin.addQuestion');
+    Route::match(['get','post'],'/storeQuestion', [SystemController::class, 'store_question'])->name('admin.storeQuestion');
+    Route::get('/assignQuestion', [SystemController::class, 'assign_question'])->name('admin.assignQuestion');
+    Route::match(['get','post'], '/getAssignQuestion', [SystemController::class, 'get_assign_quest'])->name('admin.getAssignQuestion');
+    Route::match(['get','post'], '/storeAssignQuestion', [SystemController::class, 'store_assign_quest'])->name('admin.storeAssignQuestion');
 
 
-    Route::get('/ordersRecieved', [SystemUsersController::class, 'orders_recieved'])->name('admin.ordersRecieved');
-    Route::get('/doctorsApproval', [SystemUsersController::class, 'doctors_approval'])->name('admin.doctorsApproval');
-    Route::get('/ordersConfrimed', [SystemUsersController::class, 'orders_confrimed'])->name('admin.ordersConfrimed');
-    Route::get('/ordersShiped', [SystemUsersController::class, 'orders_shiped'])->name('admin.ordersShiped');
+    Route::get('/ordersRecieved', [SystemController::class, 'orders_recieved'])->name('admin.ordersRecieved');
+    Route::get('/doctorsApproval', [SystemController::class, 'doctors_approval'])->name('admin.doctorsApproval');
+    Route::get('/ordersConfrimed', [SystemController::class, 'orders_confrimed'])->name('admin.ordersConfrimed');
+    Route::get('/ordersShiped', [SystemController::class, 'orders_shiped'])->name('admin.ordersShiped');
 
 });
