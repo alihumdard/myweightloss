48ae8041a2c79cddaaf05a92c9f505cfd5643c65
shaizi-dev <shahroimtiaz758@gmail.com>
2024-02-20 04:52:17 -0800

    changes at fitness profile device responsive , and currently skip submit handling

--- public/assets/web/bmi/js/wizard_func_multiple_branch.js
+++ public/assets/web/bmi/js/wizard_func_multiple_branch.js
@@ -1,17 +1,17 @@
 jQuery(function ($) {
     "use strict";
-    
-    // Choose here which method to send the email, available:
-    // Simple phpmail text/plain > form_send_multiple_branch.php (default)
-    // PHPMailer text/html > phpmailer/multiple_branch_phpmailer.php
-    // PHPMailer text/html SMTP > phpmailer/multiple_branch_phpmailer_smtp.php
-    // PHPMailer with html template > phpmailer/multiple_branch_phpmailer_template.php
-    // PHPMailer with html template SMTP> phpmailer/multiple_branch_phpmailer_template_smtp.php
+
+    // Set the form action attribute
     $('form#wrapped').attr('action', 'form_send_multiple_branch.php');
-    
+
+    // Check if there's a stored step in localStorage
+    var storedStep = localStorage.getItem('wizardStep');
+
+    // Initialize wizard with stored step
     var wizard = $("#wizard_container").wizard({
         stepsWrapper: "#wrapped",
         submit: ".submit",
+        startStep: storedStep ? parseInt(storedStep) : 0,
         transitions: {
             branchtype: function ($step, action) {
                 var branch = $step.find(":checked").val();
@@ -41,15 +41,41 @@ jQuery(function ($) {
         }
     });
 
-    // Add click event for the "Next Step" button
-    $('.next-step').on('click', function () {
-        // Move to the next step
-        wizard.wizard('next');
+    // Function to show or hide the submit button based on input fields
+    function toggleSubmitButton() {
+        var allFilled = true;
+        $('input.required').each(function () {
+            if ($(this).val() == '') {
+                allFilled = false;
+                return false; // exit each loop early if any field is empty
+            }
+        });
+
+        if (allFilled) {
+            $('.submit').show();
+        } else {
+            $('.submit').hide();
+        }
+    }
+
+    // Call the function when the page loads
+    toggleSubmitButton();
+
+    // Event listener for input fields
+    $('input').on('input', function () {
+        toggleSubmitButton();
     });
 
-    // Add click event for the "Backward" button
-    $('.backward').on('click', function () {
-        // Move to the previous step
-        wizard.wizard('previous');
+    // Add click event for the submit button
+    $('.submit').on('click', function (event) {
+        // Prevent the default behavior of the link
+        event.preventDefault();
+        // Submit the form
+        $('#wrapped').submit();
+    });
+
+    // Save current step to localStorage when step changes
+    wizard.on('stepchange', function (event, state) {
+        localStorage.setItem('wizardStep', state.currentIndex);
     });
 });
--- resources/views/web/pages/bmi_form.blade.php
+++ resources/views/web/pages/bmi_form.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="STEPS | Multipurpose Working Wizard with Branches">
-    <meta name="author" content="Ansonika">
-    <title>BMI Calulation Steps</title>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="STEPS | Multipurpose Working Wizard with Branches">
+        <meta name="author" content="Ansonika">
+        <title>BMI Calulation Steps</title>
         <!-- Favicons-->
         <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
         <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png') }}">
@@ -45,40 +46,40 @@
         <main>
             <div class="container ng-lightgray">
                 <div class="row">
-                    <div class="col-md-6 pt-3">
-                        <img src="{{ asset('/assets/web/bmi/img/Weighloss_final_logo.png') }}" class="bmi-logo">
-                    </div>
-                    <div class="col-md-6 pt-4 text-md-end">
+                    <div class="align-items-center data">
+                        <img src="{{ asset('/assets/web/bmi/img/Weighloss_final_logo.png') }}" class="bmi-logo"
+                            alt="Logo">
+
                         <a href="{{ route('web.logout') }}" class="btn btn-primary" data-toggle="modal"
                             data-target="#exampleModal">
-                            <i class="fa fa-sign-in" aria-hidden="true"></i>LogOut
+                            <i class="fa fa-sign-in" aria-hidden="true"></i> LogOut
                         </a>
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                            aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        ...
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary"
-                                            data-dismiss="modal">Close</button>
-                                        <button type="button" class="btn btn-primary">Save changes</button>
-                                    </div>
+                    </div>
+
+
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                        aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-            </div>
+
+                </div>
             </div>
             <div class="container">
                 <div id="wizard_container">
@@ -87,30 +88,38 @@
                         <!-- Leave input above for security protection, read docs for details -->
                         <div class="step" data-state="branchtype">
                             <div class="question_title">
-                                <h3>Let's Create Your Body Profile</h3>
+                                <h3 class="fw-bold text-center">Let's Create Your Body Profile</h3>
                                 <p><strong>Select your gender:</strong></p>
                             </div>
+
                             <div class="row">
-                                <div class="col-md-3"></div>
-                                <div class="col-lg-3 animated zoomIn delay-fast">
-                                    <div class="item">
-                                        <input id="answer_1" type="radio" name="branch_1_group_1"
-                                            value="Seo-Optimization" class="required">
-                                        <label for="answer_1"><img
-                                                src="{{ asset('/assets/web/bmi/img/weightloss/11.png') }}"
-                                                class="bmi-main-images" alt=""></label>
+                                <!-- <div class="col-md-3"></div> -->
+                                <div class="row align-items-center d-flex justify-content-center">
+                                    <div
+                                        class="col-lg-3 col-md-6 animated zoomIn delay-fast d-flex justify-content-center">
+                                        <div class="item">
+                                            <input id="answer_1" type="radio" name="branch_1_group_1"
+                                                value="Seo-Optimization" class="required">
+                                            <label for="answer_1">
+                                                <img src="{{ asset('/assets/web/bmi/img/weightloss/11.png') }}"
+                                                    class="bmi-main-images" alt="">
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div
+                                        class="col-lg-3 col-md-6 animated zoomIn delay-normal d-flex justify-content-center">
+                                        <div class="item">
+                                            <input id="answer_2" name="branch_1_group_1" type="radio"
+                                                value="Seo-Optimization" class="required">
+                                            <label for="answer_2">
+                                                <img src="{{ asset('/assets/web/bmi/img/weightloss/4(1).png') }}"
+                                                    class="bmi-main-images" alt="">
+                                            </label>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-lg-3 animated zoomIn delay-normal">
-                                    <div class="item">
-                                        <input id="answer_2" name="branch_1_group_1" type="radio"
-                                            value="Seo-Optimization" class="required">
-                                        <label for="answer_2"><img
-                                                src="{{ asset('/assets/web/bmi/img/weightloss/4(1).png') }}"
-                                                class="bmi-main-images" alt=""></label>
-                                    </div>
-                                </div>
-                                <div class="col-md-3"></div>
+
+                                <!-- <div class="col-md-3"></div> -->
                             </div>
 
                         </div>
@@ -125,95 +134,105 @@
                                     <p>ACCORDING TO YOUR AGE</p>
                                 </div>
                                 <div class="row">
-                                    <div class="col-lg-3 animated zoomIn delay-fast">
-                                        <div class="item">
+                                    <div class="col-lg-3 col-md-6 animated zoomIn delay-fast">
+                                        <div class="item text-center">
                                             <input id="branch_2_answer_1" type="radio" name="branch_1_group_1"
                                                 value="Seo-Optimization" class="required">
-                                            <label for="branch_2_answer_1"><img
-                                                    src="{{ asset('/assets/web/bmi/img/11.png') }}" alt=""><button
-                                                    type="button" name="forward" class="forward">18-25</button>
+                                            <label for="branch_2_answer_1">
+                                                <img src="{{ asset('/assets/web/bmi/img/11.png') }}" alt=""
+                                                    class="img-fluid mb-2">
+                                                <button type="button" name="forward"
+                                                    class="forward btn btn-sm btn-primary">18-25</button>
+                                            </label>
                                         </div>
                                     </div>
-                                    <div class="col-lg-3 animated zoomIn delay-normal">
-                                        <div class="item">
+                                    <div class="col-lg-3 col-md-6 animated zoomIn delay-normal">
+                                        <div class="item text-center">
                                             <input id="branch_2_answer_2" name="branch_1_group_1" type="radio"
                                                 value="Web-Development" class="required">
-                                            <label for="branch_2_answer_2"><img
-                                                    src="{{ asset('/assets/web/bmi/img/7.png') }}" alt=""><button
-                                                    type="button" name="forward" class="forward">26-35</button>
+                                            <label for="branch_2_answer_2">
+                                                <img src="{{ asset('/assets/web/bmi/img/7.png') }}" alt=""
+                                                    class="img-fluid mb-2">
+                                                <button type="button" name="forward"
+                                                    class="forward btn btn-sm btn-primary">26-35</button>
+                                            </label>
                                         </div>
                                     </div>
-                                    <div class="col-lg-3 animated zoomIn delay-medium">
-                                        <div class="item">
+                                    <div class="col-lg-3 col-md-6 animated zoomIn delay-medium">
+                                        <div class="item text-center">
                                             <input id="answer_3" name="branch_1_group_1" type="radio" value="Design"
                                                 class="required">
-                                            <label for="answer_3"><img src="{{ asset('/assets/web/bmi/img/10.png') }}"
-                                                    alt=""><button type="button" name="forward"
-                                                    class="forward">36-45</button>
+                                            <label for="answer_3">
+                                                <img src="{{ asset('/assets/web/bmi/img/10.png') }}" alt=""
+                                                    class="img-fluid mb-2">
+                                                <button type="button" name="forward"
+                                                    class="forward btn btn-sm btn-primary">36-45</button>
+                                            </label>
                                         </div>
                                     </div>
-                                    <div class="col-lg-3 animated zoomIn delay-medium">
-                                        <div class="item">
+                                    <div class="col-lg-3 col-md-6 animated zoomIn delay-medium">
+                                        <div class="item text-center">
                                             <input id="answer_4" name="branch_1_group_1" type="radio" value="Design"
                                                 class="required">
-                                            <label for="answer_4"><img src="{{ asset('/assets/web/bmi/img/4.png') }}"
-                                                    alt="" class="bg-white"><button type="button" name="forward"
-                                                    class="forward">46+</button>
+                                            <label for="answer_4">
+                                                <img src="{{ asset('/assets/web/bmi/img/4.png') }}" alt=""
+                                                    class="img-fluid bg-white mb-2">
+                                                <button type="button" name="forward"
+                                                    class="forward btn btn-sm btn-primary">46+</button>
+                                            </label>
                                         </div>
                                     </div>
                                 </div>
 
+
                             </div>
                             <!-- /step -->
                         </div>
-                        
+
 
 
                         <!-- /Web-Development > FrontEnd-Development ============================== -->
 
                         <!-- Design > What Type of Design do you need ============================== -->
+                        <!-- Your HTML code -->
                         <div class="branch" id="Design">
-
-
-                            <!-- Budget ============================== -->
+                            <!-- Budget -->
                             <div class="step" id="budget" data-state="">
                                 <div class="question_title">
                                     <h3>BMI Calculator</h3>
-                                    <p>What is your weighi and height</p>
+                                    <p>What is your weight and height</p>
                                 </div>
                                 <div class="row justify-content-center">
-                                    <div class="col-lg-5 animated zoomIn delay-fast">
+                                    <div class="col-lg-6 col-md-8 col-sm-10 animated zoomIn delay-fast">
                                         <div class="items">
-                                            <div class="form-group">
-                                                <label for="height" class="mb-3 text-left f-bold">Height</label>
-                                                <input type="number" class="form-control" id="height" placeholder="cm"
-                                                    required>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="weight" class="mt-3 text-left f-bold">Weight</label>
-                                                <input type="number" class="form-control" id="weight" placeholder="kg"
-                                                    required>
+                                            <div class="row m-2">
+                                                <div class="col-12">
+                                                    <div class="form-group">
+                                                        <label for="height"
+                                                            class="mb-3 text-left font-weight-bold">Height</label>
+                                                        <input type="number" class="form-control" id="height"
+                                                            placeholder="cm" required>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12">
+                                                    <div class="form-group">
+                                                        <label for="weight"
+                                                            class="mt-3 text-left font-weight-bold">Weight</label>
+                                                        <input type="number" class="form-control" id="weight"
+                                                            placeholder="kg" required>
+                                                    </div>
+                                                </div>
                                             </div>
 
-                                            <a href="{{ route('web.consultationForm')}}">
-                                                <button type="button"
-                                                    class="btn btn-info text-light next-step">Continue</button>
-                                            </a>
                                         </div>
-
                                     </div>
-
                                 </div>
-
-                                <!-- /Budget ============================== -->
                             </div>
                         </div>
                         <div id="bottom-wizard">
-                            <button type="button" name="backward" class="backward">Backward </button>
-                            <a href="/index.html"><button type="button" name="forward"
-                                    class="forward">Continue</button></a>
-
-                            <button type="submit" name="process" class="submit">Submit</button>
+                            <button type="button" name="backward" class="backward btn btn-secondary">Backward</button>
+                            <button type="button" name="forward" class="forward btn btn-primary">Continue</button>
+                            <a href="{{ route('web.consultationForm') }}"></a>
                         </div>
                     </form>
                 </div>
