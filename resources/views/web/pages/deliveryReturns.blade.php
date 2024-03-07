cc5e98c9021e51161ff4c5afb3f11c5f2207f57e
shaizi-dev <shahroimtiaz758@gmail.com>
2024-03-07 12:31:53 -0800

    add changes

--- resources/views/web/pages/deliveryReturns.blade.php
+++ resources/views/web/pages/deliveryReturns.blade.php
@@ -1,3 +1,7 @@
+@extends('web.layouts.default')
+@section('title', 'deliveryReturns')
+@section('content')
+
 <!doctype html>
 <html lang="en">
 
@@ -6,10 +10,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Delivery & Returns</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/assets/web/css/style.css') }}">
     <style>
-        body {
-            padding: 20px;
-        }
+        
 
         h2,
         h3 {
@@ -29,11 +32,11 @@
 
 <body>
     <div class="container">
-        <div class="row">
+        <!-- <div class="row">
             <div class="col">
-                <h2 class="text-center">Delivery & Returns</h2>
+                <h2 class="text-center mt-5 mb-5">Delivery & Returns</h2>
             </div>
-        </div>
+        </div> -->
         <div class="row">
             <div class="col">
                 <h3>My Weight Loss Centre Delivery, Returns & Refunds Policy</h3>
@@ -122,3 +125,11 @@
 </html>
 
 </html>
+
+@stop
+
+@pushOnce('scripts')
+<script>
+
+</script>
+@endPushOnce
--- resources/views/web/pages/howitworks.blade.php
+++ resources/views/web/pages/howitworks.blade.php
@@ -1,3 +1,7 @@
+@extends('web.layouts.default')
+@section('title', 'deliveryReturns')
+@section('content')
+
 <!doctype html>
 <html lang="en">
 
@@ -6,9 +10,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>how it work</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('/assets/web/css/style.css') }}">
     <style>
-        /* Custom Styles */
+       
         .container {
             margin-top: 50px;
         }
@@ -19,36 +23,36 @@
 <body>
     <div class="container col-12 justify-content-center">
         
-        <h4>How It Works</h4>
+        <h5>How It Works</h5>
         <p>You no longer have to wait for GP appointments. Our services are fast, simple, and easy to use. Receive genuine medication delivered conveniently and discreetly packaged to your door. Here at My Weight Loss Centre.</p>
         <!-- <img src="{{ asset('/assets/web/images/tsp/recent-articles/1.png') }}" alt="post image" loading="lazy"> -->
-        <h4>1. Start a FREE* 60 Seconds online consultation</h4>
+        <h5>1. Start a FREE* 60 Seconds online consultation</h5>
         <p>Our streamlined process focuses on the most crucial aspects of your weight management journey, ensuring a rapid yet comprehensive overview of your situation. By asking targeted questions about your current health, lifestyle, and weight loss aspirations, we swiftly assess key factors to kickstart your journey towards a healthier you.</p>
-        <h4>Fast and Simple.</h4>
+        <h5>Fast and Simple.</h5>
         <p>Our online FREE* consultation only takes a few quick seconds, fast, simple & easy to complete. Our short few questions are similar to those that would be asked at your GP or Weight Loss specialist appointment.</p>
 
-        <h4>Select Your Treatment</h4>
+        <h5>Select Your Treatment</h5>
         <p>All of our experienced UK Independent prescribers are registered with the General Pharmaceuticals Council (GPhC). They will review your consultation immediately. Once approved, Weight Loss Centre medical team will then recommend a treatment and issue a FREE* prescription.</p>
 
-        <h4>Genuine Treatments</h4>
+        <h5>Genuine Treatments</h5>
         <p>We only stock 100% regulated and approved medications.</p>
 
-        <h4>Top Leading Brands</h4>
+        <h5>Top Leading Brands</h5>
         <p>We have a huge selection of top well-known brands.</p>
 
-        <h4>FREE* Prescription</h4>
+        <h5>FREE* Prescription</h5>
         <p>All our services from consultation to prescription are FREE* (please read our terms and conditions).</p>
 
-        <h4>Receive Your Delivery</h4>
+        <h5>Receive Your Delivery</h5>
         <p>All orders before 3pm are dispatched the very same day in discreet packaging, keeping you notified every step of the way until your order is received.</p>
 
-        <h4>Flexible Express Delivery</h4>
+        <h5>Flexible Express Delivery</h5>
         <p>We have multiple delivery options which you can choose from on checkout, from FREE delivery* to express next day delivery*.</p>
 
-        <h4>Discreet Packaging</h4>
+        <h5>Discreet Packaging</h5>
         <p>Each and every order we dispatch is packaged discreetly for your peace of mind and is delivered to your chosen destination.</p>
 
-        <h4>REVIEWS.io Approved (Top Rated)</h4>
+        <h5>REVIEWS.io Approved (Top Rated)</h5>
         <p>Once you have received your medications and are happy with it, we would love to hear from you. Every review is seen by our Weight Loss Centre team and the World Wide Web. We can't wait to hear from you!</p>
 
     </div>
@@ -57,3 +61,11 @@
 </body>
 
 </html>
+
+@stop
+
+@pushOnce('scripts')
+<script>
+
+</script>
+@endPushOnce
