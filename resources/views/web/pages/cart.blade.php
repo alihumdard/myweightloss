bd97141ae42750227cc095ff471f912b82d9535a
Usman <it.dev45@dawaateislami.net>
2024-03-13 22:05:52 +0500

    checkout page work

--- resources/views/web/pages/cart.blade.php
+++ resources/views/web/pages/cart.blade.php
@@ -106,14 +106,14 @@
             <div class="row">
               <div class="col-md-6 mb-3">
                 <label for="firstName">First name</label>
-                <input type="text" name="firstName" class="form-control" id="firstName" required>
+                <input type="text" name="firstName" class="form-control" id="firstName" >
                 <div class="invalid-feedback">
                   Valid first name is required.
                 </div>
               </div>
               <div class="col-md-6 mb-3">
                 <label for="lastName">Last name</label>
-                <input type="text" name="lastName" class="form-control" id="lastName" required>
+                <input type="text" name="lastName" class="form-control" id="lastName" >
                 <div class="invalid-feedback">
                   Valid last name is required.
                 </div>
@@ -130,7 +130,7 @@
   
             <div class="mb-3">
               <label for="address">Address</label>
-              <input type="text" name="address" class="form-control" id="address" placeholder="1234 Main St" required>
+              <input type="text" name="address" class="form-control" id="address" placeholder="1234 Main St">
               <div class="invalid-feedback">
                 Please enter your shipping address.
               </div>
@@ -161,7 +161,7 @@
             </div> --}}
             <div class="form-check">
               <div class="custom-control">
-                <input class="form-check-input" type="radio" name="shipping_method" id="express_delivery" value="express" data-ship="3.95">
+                <input class="form-check-input" type="radio" name="shipping_method" id="express_delivery" value="express" checked data-ship="3.95">
                 <label class="form-check-label" for="express_delivery">Royal Mail Tracked 24</label>
                 <span class="float-right">£3.95</span>
               </div>
@@ -177,9 +177,9 @@
             </div>
           </div>
 
-          <hr class="mb-4">
+          {{-- <hr class="mb-4"> --}}
   
-          <h4 class="mb-3">Payment</h4>
+          {{-- <h4 class="mb-3">Payment</h4>
           <div class="row">
             <div class="col-md-6 mb-3">
               <label for="cc-name">Name on card</label>
@@ -191,16 +191,16 @@
             </div>
             <div class="col-md-6 mb-3">
               <label for="cc-number">Card number</label>
-              <input type="text" name="cardNumber" class="form-control" id="cc-number" required>
+              <input type="text" name="cardNumber" class="form-control onlyDigitsInput" id="cc-number" required>
               <div class="invalid-feedback">
                 Card number is required
               </div>
             </div>
-          </div>
-          <div class="row">
+          </div> --}}
+          {{-- <div class="row">
             <div class="col-md-3 mb-3">
               <label for="cc-expiration">Expiration</label>
-              <input type="text" id="cardExp" name="cardExp" placeholder="MM/YY" required>
+              <input type="text" id="cardExp" name="cardExp" class="form-control" placeholder="MM/YY" required>
               <div class="invalid-feedback">
                 Expiration date required
               </div>
@@ -212,11 +212,11 @@
                 Security code required
               </div>
             </div>
-          </div>
+          </div> --}}
           <hr class="mb-4">
           <input type="hidden" name="total_ammount" class="total-hidden" value="{{ $total ?? 0}}">
           <input type="hidden" name="product_desc" value="{{ $value['product']['title'] ?? ''}}">
-          <button  type="submit" class="btn btn__primary checkout-btn">Proceed To Checkout</button>
+          <button  type="submit" class="btn btn__secondary checkout-btn">Proceed To Checkout</button>
         </form>
       </div>
       
@@ -244,6 +244,12 @@
 @pushOnce('scripts')
 <script>
   $(document).ready(function() {
+    var shippingCost = $('input[name="shipping_method"]:checked').data('ship');
+      var price = (parseInt($('.cart-product__price').text()) * parseInt($('.qty-input').val())) + shippingCost;
+      $('.cart-product__total').text(price);
+      $('.checkout-btn').text('Proceed To Checkout  ($' + price + ')');
+      $('.total-hidden').val(price);
+      
     $('input[name="shipping_method"]').change(function() {
       var shippingCost = $('input[name="shipping_method"]:checked').data('ship');
       var price = (parseInt($('.cart-product__price').text()) * parseInt($('.qty-input').val())) + shippingCost;
@@ -253,12 +259,26 @@
     });
 
     $('#use_different_address').change(function() {
-            if ($(this).is(":checked")) {
-                $('.shipping-address-div').show();
-            } else {
-                $('.shipping-address-div').hide();
-            }
-        });
+      if ($(this).is(":checked")) {
+          $('.shipping-address-div').show();
+          $('#firstName').prop('required', true);
+          $('#lastName').prop('required', true);
+          $('#address').prop('required', true);
+      } else {
+          $('.shipping-address-div').hide();
+          $('#firstName').prop('required', false);
+          $('#lastName').prop('required', false);
+          $('#address').prop('required', false);
+      }
+    });
+
+    $('.onlyDigitsInput').on('keypress', function(event) {
+        // Check if the key pressed is a digit or the backspace key
+        if (event.which < 48 || event.which > 57) {
+          // Prevent default action if the key pressed is not a digit or backspace
+          event.preventDefault();
+        }
+      });
   });
 </script>
 @endPushOnce
